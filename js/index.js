46a97e35a484ebb091094b0e54627e9b8f65278a
Pau-za <molo_pau@hotmail.com>
2019-06-02 20:42:58 -0500

    inicializa proyecto ciencia para el pueblo. Agrega imagen del fondo de la página, y el navbar blanco con letras negras. Falta hacer la función que haga que cuando el usuario scrollee la página y el nav llegue hasta arriba, se quede fijo, y la imagen se cubra por completo, pero que cuando esccrolee hacia abajo vuelva a la posición en la que se ve la imagen

---
+++ js/index.js
@@ -0,0 +1,29 @@
+// Acá debo hacer una función que cambie la clase normal-nav
+// del div externo al nav, por una navbar-fixed, cuando
+// llegue a la parte superior de la página. Para que se
+// quede ahí mismo, pero, si el usuario scrollea hacia abajo, que se vuelva a bajar
+// hasta que ya no pueda más porque está la imagen del fondo
+
+// La siguiente es una función que me puede ayudar a hacerlo:
+
+window.onscroll = function() {scrollFunction()};
+
+const buttonToTop = document.getElementById('button-to-top');
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    buttonToTop.style.display = "block";
+  } else {
+    buttonToTop.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+buttonToTop.addEventListener('click', () => {
+  topFunction();
+})
